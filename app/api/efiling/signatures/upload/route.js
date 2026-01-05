1a87c6d4843185a6337c7ff112c1b6f24231086a
abdullah7175 <abdullah7175@gmail.com>
2026-01-05 16:02:10 +0500

    comit

--- app/api/efiling/signatures/upload/route.js
+++ app/api/efiling/signatures/upload/route.js
@@ -120,6 +120,7 @@ export async function POST(request) {
             
             // Store mimeType for database
             fileType = mimeType;
+        }
 
         // Save signature info to database with signature management
         const client = await connectToDatabase();
