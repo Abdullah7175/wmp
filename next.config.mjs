75fb36495337a6e32d339fec73b264d434135262
abdullah7175 <abdullah7175@gmail.com>
2026-01-08 19:11:18 +0500

    comit

--- next.config.mjs
+++ next.config.mjs
@@ -100,10 +100,6 @@ const nextConfig = {
             value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https: http:; font-src 'self' data:; connect-src 'self' https: http:; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content;"
           },
           {
-            key: 'Content-Type',
-            value: 'text/html; charset=UTF-8'
-          },
-          {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains'
           },
